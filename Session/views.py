29249c2b7ed94d311feb6c2a3dc1938d1ea5c835
rc253244 <raphael.castanier@cea.fr>
2020-05-01 12:49:54 +0200

    Add JSON content and load

--- Session/views.py
+++ Session/views.py
@@ -1,5 +1,14 @@
+import logging
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+def load_content():
+    with open('Session/content.json') as f:
+        return json.load(f)
+
+content = load_content()
 
 
 def home(request):
