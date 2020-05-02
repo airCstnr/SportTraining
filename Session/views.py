9ef62e63ff78712b4a26ef5c46b37390acedce12
rc253244 <raphael.castanier@cea.fr>
2020-05-02 11:00:04 +0200

    Add most of interface

--- Session/views.py
+++ Session/views.py
@@ -1,15 +1,14 @@
-import logging
 import json
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def load_content():
     with open('Session/content.json') as f:
         return json.load(f)
 
-content = load_content()
-
 
 def home(request):
-    return render(request, 'Session/home.html')
+    content = load_content()
+    return render(request, 'Session/home.html', content)
